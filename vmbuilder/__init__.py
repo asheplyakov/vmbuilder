a224b676c43a583227a5fc6f97bb6422d0c8221e
Alexey Sheplyakov <asheplyakov@altlinux.org>
2018-08-31 16:45:24 +0400

    [trivia] Moved template into the package directory

--- vmbuilder/__init__.py
+++ vmbuilder/__init__.py
@@ -4,4 +4,4 @@ from __future__ import absolute_import
 import os
 
 PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))
-TEMPLATE_DIR = os.path.join(os.path.dirname(PACKAGE_DIR), 'templates')
+TEMPLATE_DIR = os.path.join(PACKAGE_DIR, 'templates')
